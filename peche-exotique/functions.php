3e785e23220e89e3bda057028f6976abfb4b2c73
Régis Daum <contact@graphandco.com>
2026-03-22 11:38:56 +0100

    Add login assets for Peche Exotique

--- peche-exotique/functions.php
+++ peche-exotique/functions.php
@@ -21,7 +21,7 @@ function custom_login_logo() {
         }
 
         .login h1 a {
-            background-image: url('$logo');
+            background-image: url('$logo') !important;
             background-size: contain;
             background-repeat: no-repeat;
             width: 300px;
