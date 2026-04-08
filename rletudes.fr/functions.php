e8a6e39f00d290bff4289337a536869e84e58364
Régis Daum <contact@graphandco.com>
2026-04-08 17:39:27 +0200

    Removed menu items

--- rletudes.fr/functions.php
+++ rletudes.fr/functions.php
@@ -20,3 +20,12 @@ add_filter('login_headerurl', function () {
 add_filter('login_headertext', function () {
     return get_bloginfo('name');
 });
+
+// HIDE ADMIN MENUS FOR EDITORS
+function gc_hide_admin_menus_for_editors() {
+    if (current_user_can('editor') && !current_user_can('administrator')) {
+        remove_menu_page('tools.php'); // Outils
+        remove_menu_page('wpseo_dashboard'); // Yoast SEO
+    }
+}
+add_action('admin_menu', 'gc_hide_admin_menus_for_editors', 999);
